5045ab2b99a37acd1de9d99ada0892087aa389c6
AntonMoskvinov <128958172+AntonMoskvinov@users.noreply.github.com>
2023-07-08 08:01:53 +0300

    Update Program.cs

--- ConsoleApp10/Program.cs
+++ ConsoleApp10/Program.cs
@@ -7,24 +7,20 @@ class Program
     static void Main()
     {
         Console.OutputEncoding = Encoding.UTF8;
-        Console.Write("Введите выражение(пример: \"2 + 3 * 5\"): ");
+        Console.Write("Введите выражение(пример: \"2 + 2 * 2\"): ");
         var expression = Console.ReadLine();
         Console.WriteLine("{0} = {1}", expression, Calc(expression));
         Console.ReadLine();
     }
 
-    /// <summary>
-    /// Калькулятор выражения
-    /// </summary>
-    /// <param name="mathExpression">Математическое выражение</param>
-    /// <returns></returns>
+  
     static double Calc(string mathExpression)
     {
-        var parts = mathExpression.Split(' '); //разбиваем выражение на части
+        var parts = mathExpression.Split(' '); 
 
         var operands = new List<double>();
         var operations = new List<string>();
-        //в цикле разделяем операнды и операции
+      
         for (var i = 0; i < parts.Length; i += 2)
         {
             operands.Add(Convert.ToDouble(parts[i]));
@@ -48,11 +44,11 @@ class Program
     {
         while (true)
         {
-            //ищем индексы текущих операций
+            
             var i1 = operations.IndexOf(currentOperation1);
             var i2 = operations.IndexOf(currentOperation2);
 
-            int index; //индекс операции которую будем вычислять
+            int index; 
             if (i1 >= 0 && i2 >= 0)
             {
                 index = Math.Min(i1, i2);
@@ -62,13 +58,13 @@ class Program
                 index = Math.Max(i1, i2);
             }
 
-            //если операции нет в списке - выходим из цикла
+        
             if (index < 0)
             {
                 break;
             }
 
-            //вычисление текущей операции
+           
             if (index == i1)
             {
                 operands[index] = function1(operands[index], operands[index + 1]);
@@ -77,9 +73,9 @@ class Program
             {
                 operands[index] = function2(operands[index], operands[index + 1]);
             }
-            //удаление операции
+           
             operations.RemoveAt(index);
-            //удаление операнда
+            
             operands.RemoveAt(index + 1);
         }
     }
